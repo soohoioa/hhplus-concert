39633fdbeb5062dceaaef1b42e6d7b484692b05f
soohoioa <soohoioa@gmail.com>
2026-01-19 14:23:18 +0900

    [chore]Redis 캐시 설정 및 TTL 정책 적용

--- src/main/java/kr/hhplus/be/server/common/cache/CacheConfig.java
+++ src/main/java/kr/hhplus/be/server/common/cache/CacheConfig.java
@@ -1,11 +1,19 @@
 package kr.hhplus.be.server.common.cache;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -18,12 +26,36 @@ public class CacheConfig {
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory cf) {
 
-        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
-                .disableCachingNullValues();
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        // 타입 정보 포함 (다형성/컬렉션/Wrapper 대응)
+        om.activateDefaultTyping(
+                LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
+
+        GenericJackson2JsonRedisSerializer valueSerializer =
+                new GenericJackson2JsonRedisSerializer(om);
+
+        RedisCacheConfiguration defaultConfig =
+                RedisCacheConfiguration.defaultCacheConfig()
+                        .disableCachingNullValues()
+                        .serializeKeysWith(
+                                RedisSerializationContext.SerializationPair
+                                        .fromSerializer(new StringRedisSerializer())
+                        )
+                        .serializeValuesWith(
+                                RedisSerializationContext.SerializationPair
+                                        .fromSerializer(valueSerializer)
+                        )
+                        .computePrefixWith(name -> "hhplus:" + name + "::");
 
         Map<String, RedisCacheConfiguration> configs = new HashMap<>();
-
-        configs.put("concert:schedules", defaultConfig.entryTtl(Duration.ofSeconds(60)));
+        configs.put("concert:schedules", defaultConfig.entryTtl(Duration.ofMinutes(10)));
+        configs.put("concert:available-seats", defaultConfig.entryTtl(Duration.ofSeconds(15)));
 
         return RedisCacheManager.builder(cf)
                 .cacheDefaults(defaultConfig)
