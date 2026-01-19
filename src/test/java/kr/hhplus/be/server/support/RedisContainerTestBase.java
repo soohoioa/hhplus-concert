1f291af67da59e831648b471bfa526d7efb6a35d
soohoioa <soohoioa@gmail.com>
2026-01-19 15:39:41 +0900

    [test]Redis 기반 캐시 통합 테스트 환경 구성

--- src/test/java/kr/hhplus/be/server/support/RedisContainerTestBase.java
+++ src/test/java/kr/hhplus/be/server/support/RedisContainerTestBase.java
@@ -1,23 +1,36 @@
 package kr.hhplus.be.server.support;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public abstract class RedisContainerTestBase {
 
-    static final GenericContainer<?> REDIS =
-            new GenericContainer<>("redis:7-alpine").withExposedPorts(6379);
-
-    static {
-        REDIS.start();
-    }
+    @Container
+    static GenericContainer<?> redis =
+            new GenericContainer<>("redis:7-alpine")
+                    .withExposedPorts(6379);
 
     @DynamicPropertySource
-    static void overrideProps(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.redis.host", REDIS::getHost);
-        registry.add("spring.data.redis.port", () -> REDIS.getMappedPort(6379));
+    static void redisProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.host", redis::getHost);
+        registry.add("spring.redis.port", () -> redis.getMappedPort(6379));
+    }
+
+    @Autowired
+    RedisConnectionFactory redisConnectionFactory;
+
+    @BeforeEach
+    void flushRedis() {
+        try (RedisConnection conn = redisConnectionFactory.getConnection()) {
+            conn.flushAll();
+        }
     }
 }
