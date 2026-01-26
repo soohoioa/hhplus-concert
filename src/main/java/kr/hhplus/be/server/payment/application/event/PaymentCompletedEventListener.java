6a8db7d2383ba1f067292646fb3fa78febea8ba7
soohoioa <soohoioa@gmail.com>
2026-01-26 15:24:21 +0900

    [refactor]결제 완료 이벤트 리스너를 Outbox 적재 방식으로 변경 - 결제 트랜잭션 이후 외부 시스템 직접 호출 로직 제거 - 결제 성공 시 Outbox에 이벤트를 저장하도록 책임 변경 - 외부 전송 실패로 인한 데이터 불일치 가능성 감소

--- src/main/java/kr/hhplus/be/server/payment/application/event/PaymentCompletedEventListener.java
+++ src/main/java/kr/hhplus/be/server/payment/application/event/PaymentCompletedEventListener.java
@@ -1,19 +1,18 @@
 package kr.hhplus.be.server.payment.application.event;
 
-import kr.hhplus.be.server.payment.port.out.SendReservationToDataPlatformPort;
+import kr.hhplus.be.server.payment.port.out.SavePaymentOutboxPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @RequiredArgsConstructor
 @Component
 public class PaymentCompletedEventListener {
 
-    private final SendReservationToDataPlatformPort sendReservationToDataPlatformPort;
+    private final SavePaymentOutboxPort savePaymentOutboxPort;
 
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @TransactionalEventListener
     public void on(PaymentCompletedEvent event) {
-        sendReservationToDataPlatformPort.send(event);
+        savePaymentOutboxPort.save(event);
     }
 }
