d62e229f6439379bf9203f241778690829e300a9
soohoioa <soohoioa@gmail.com>
2026-02-24 12:19:40 +0900

    [chore] 사용하지 않는 import 제거

--- src/main/java/kr/hhplus/be/server/payment/application/event/PaymentCompletedEventListener.java
+++ src/main/java/kr/hhplus/be/server/payment/application/event/PaymentCompletedEventListener.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @RequiredArgsConstructor
